ff4db51ce68d676031a547dd0ee31b5591af0bbe
ZiadRefaey <ziad.refaey1@gmail.com>
2024-10-30 01:47:16 +0300

    chore: deleting unused file

--- app/_components/UI/Input.tsx
+++ app/_components/UI/Input.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-import React, { InputHTMLAttributes, ReactNode } from "react";
+import React, { ReactNode } from "react";
 
 type HTMLInputTypeAttribute =
   | "button"
