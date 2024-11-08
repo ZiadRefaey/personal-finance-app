a4d3aa21fa3d8a0f4939ee40bf19c618c3a2f077
ZiadRefaey <ziad.refaey1@gmail.com>
2024-11-08 03:25:50 +0200

    feat(overview): displaying higest 4 saved pots in pots overview

--- app/_components/PotsOverview.tsx
+++ app/_components/PotsOverview.tsx
@@ -3,19 +3,24 @@ import Card from "./UI/Card";
 import Pot from "@/public/icon-pot.svg";
 import Image from "next/image";
 import OverviewSectionHeader from "./OverviewSectionHeader";
+import { getPots } from "../_lib/data-service";
+import { auth } from "@/auth";
 
 type PotDetailsType = {
   title: string;
   amount: number;
   color: string;
 };
-const PotDetailsArray: PotDetailsType[] = [
-  { title: "Savings", amount: 159, color: "bg-green" },
-  { title: "Gift", amount: 40, color: "bg-cyan" },
-  { title: "Concert Ticket", amount: 110, color: "bg-navy" },
-  { title: "New Laptop", amount: 10, color: "bg-yellow" },
-];
-export default function PotsOverview() {
+
+export default async function PotsOverview() {
+  const session = await auth();
+  const userId = Number(session?.user?.id);
+  const pots = await getPots(userId);
+  const totalSaved = pots.reduce((acc, cur) => cur.saved + acc, 0);
+  const HighestSavedPots = pots
+    .toSorted((a: any, b: any) => b.saved - a.saved)
+    .slice(0, 4);
+
   return (
     <Card>
       <OverviewSectionHeader
@@ -33,17 +38,17 @@ export default function PotsOverview() {
           <Image src={Pot} alt="Pot Icon" />
           <div className="">
             <p className="text-secondary text-preset-4 mb-3">Total Saved</p>
-            <p className="text-primary text-preset-1">$850</p>
+            <p className="text-primary text-preset-1">${totalSaved}</p>
           </div>
         </div>
 
         <div className="grid grid-cols-2 grid-rows-2 gap-4">
-          {PotDetailsArray.map((obj) => (
+          {HighestSavedPots.map((pot) => (
             <PotDetails
-              amount={obj.amount}
-              key={obj.title}
-              color={obj.color}
-              title={obj.title}
+              amount={pot.saved}
+              key={pot.id}
+              color={pot.color}
+              title={pot.title}
             />
           ))}
         </div>
@@ -54,7 +59,10 @@ export default function PotsOverview() {
 function PotDetails({ title, amount, color }: PotDetailsType) {
   return (
     <div className="flex items-center justify-start">
-      <div className={`rounded-lg ${color} w-1 h-full mr-4`}></div>
+      <div
+        className={`rounded-lg w-1 h-full mr-4`}
+        style={{ backgroundColor: `var(--${color})` }}
+      ></div>
       <div>
         <p className="text-secondary text-preset-5">{title}</p>
         <p className="text-preset-4-bold text-primary">${amount}</p>
