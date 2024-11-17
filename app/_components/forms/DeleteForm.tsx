f46942987fa884a83a643ec2e389079769af2c5d
ZiadRefaey <ziad.refaey1@gmail.com>
2024-11-17 02:39:45 +0200

    feat(delete form): loading indicator when submitting

--- app/_components/forms/DeleteForm.tsx
+++ app/_components/forms/DeleteForm.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import Button from "../UI/Button";
 import { toast } from "@/hooks/use-toast";
 import { useModal } from "../Modal";
+import { useFormState, useFormStatus } from "react-dom";
 type ActionFunction = (id: number) => Promise<void | string>;
 
 export default function DeleteForm({
@@ -29,12 +30,7 @@ export default function DeleteForm({
       action={clientAction}
       className="w-full flex items-center justify-center flex-col"
     >
-      <Button
-        className="bg-red text-white hover:opacity-70 mb-3 w-full hover:bg-red"
-        type="submit"
-      >
-        Yes, Confirm Deletion
-      </Button>
+      <Submit />
       <button
         type="reset"
         className="py-2 text-secondary hover:text-primary duration-150 transition-all w-full"
@@ -42,5 +38,16 @@ export default function DeleteForm({
         No, Go Back
       </button>
     </form>
+  );
+}
+function Submit() {
+  const { pending } = useFormStatus();
+  return (
+    <Button
+      className="bg-red text-white hover:opacity-70 mb-3 w-full hover:bg-red"
+      type="submit"
+    >
+      {pending ? "Deleting..." : "Yes, Confirm Deletion"}
+    </Button>
   );
 }
