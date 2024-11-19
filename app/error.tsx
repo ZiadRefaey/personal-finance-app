756498cc1bd7941a2995578ea7a81dd3ad6ba0dc
ZiadRefaey <ziad.refaey1@gmail.com>
2024-11-19 07:21:09 +0200

    style:error handling page

---
+++ app/error.tsx
@@ -0,0 +1,26 @@
+"use client"; // Error boundaries must be Client Components
+
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <p>{error.message}</p>
+      <p>if the issue persists please contact the developer</p>
+      <Link href={"/"}>Home Page</Link>
+    </div>
+  );
+}
