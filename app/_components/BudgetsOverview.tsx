dc02318623e921e27fcc3a2e4b936cdecde8676b
ZiadRefaey <ziad.refaey1@gmail.com>
2024-12-15 00:51:50 +0200

    style: placeholder in budgets overview whn no transactions are made within any budget

--- app/_components/BudgetsOverview.tsx
+++ app/_components/BudgetsOverview.tsx
@@ -12,6 +12,8 @@ export default async function BudgetsOverview() {
   const session = await auth();
   const budgets = await getBudgets(Number(session?.user?.id));
   const budgetsWithSpent = await getBudgetsWithSpent(budgets);
+  const totalSpent = budgetsWithSpent.reduce((acc, cur) => acc + cur.spent, 0);
+
   //descending sorting based on budget spent. then displaying the top 4
   const displayedBudgets = budgetsWithSpent
     .toSorted((a, b) => b.spent - a.spent)
@@ -32,7 +34,15 @@ export default async function BudgetsOverview() {
       )}
       {budgetsWithSpent.length > 0 && (
         <div className="flex items-center justify-center flex-col">
-          <PieChartShad data={displayedBudgets} />
+          {totalSpent == 0 ? (
+            <div className="w-full items-center justify-center flex h-[290px] 2xl:h-[340px]">
+              <p className="text-preset-2 text-primary">
+                No transactions made yet.
+              </p>
+            </div>
+          ) : (
+            <PieChartShad data={displayedBudgets} />
+          )}
           <BudgetSpendingSummaryDetails data={displayedBudgets} />
         </div>
       )}
