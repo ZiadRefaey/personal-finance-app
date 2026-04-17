5aeda5891496b7ff38a1e300c5eb990c5be7a1ec
Ziad Refaey <ziad.refaey1@gmail.com>
2026-04-17 15:50:00 +0200

    updating supabase project

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "kqlpjtnjqdelklhbemkw.supabase.co",
+        hostname: "tpcrxlmplhkmougponql.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/**",
       },
